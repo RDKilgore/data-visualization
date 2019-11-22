175025ff7b539015e7bda4e2d61eadca1dec6439
Rian <RDKilgore@gmail.com>
2019-11-21 20:32:01 -0800

    Updates to chart code are made

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
   const BASE_URL = "https://zagster-service.herokuapp.com"
 
+$(updateGraph)
 $(updateView)
-
 function updateView() {
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 }
@@ -9,4 +9,26 @@ function updateView() {
 function updateRideCount(data) {
   numberOfRides = data.count
   $("h3#rideCount").html(numberOfRides)
+}
+
+function updateGraph() {
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
 }
