f27ce29e0f4639ef0abf25b3530d3db6ffec0074
Rian <RDKilgore@gmail.com>
2019-11-26 21:39:45 -0800

    updates to try fix on discord

--- main.js
+++ main.js
@@ -44,19 +44,20 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 // console.log (year_list[2][11])
 // console.log (year_list[3][12])
 
-$(updateView)
-
+// $(updateView)
+$(updateGraph)
 //array to hold data points pulled from jQuery
-let years= []
-let months2016 = []
-let months2017 = []
-let months2018 = []
+
+// let years= []
+// let months2016 = []
+// let months2017 = []
+// let months2018 = []
 
 //define or implement a function
 function updateView(){
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear),
-  ).then(updateChart);
+  
+  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear),).then(updateChart);
 }
 
 function perYear(data) {
@@ -87,25 +88,24 @@ function perYear(data) {
 
 // console.log(numberOfRides)
 
-// //creates chart 
-// function updateGraph() {
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var chart = new Chart(ctx, {
-//     // The type of chart we want to create
-//     type: 'line',
+//creates chart 
+function updateGraph() {
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
 
-//     // The data for our dataset
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     },
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
 
 //     // Configuration options go here
 //     options: {}
 // });
-// }
