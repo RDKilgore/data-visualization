19060d4bafd85bd358cc712ddd873fc58503cf64
Rian <RDKilgore@gmail.com>
2019-11-26 21:03:06 -0800

    adds final changes from class

--- main.js
+++ main.js
@@ -1,49 +1,80 @@
-// grabbing data from this site //memory location that never changes
+// // grabbing data from this site //memory location that never changes
 const BASE_URL = "https://zagster-service.herokuapp.com"
-const PI = 3.14159
+// const PI = 3.14159
 
-//jQuery command. wait until web page loads call function 
-//whos name is in parantheses
-//function call means run the code
-// $(updateGraph)
-// $(updateView)
-// $(greeter)
+// //jQuery command. wait until web page loads call function 
+// //whos name is in parantheses
+// //function call means run the code
+// // $(updateGraph)
+// // $(updateView)
+// // $(greeter)
 
-// call function add (use it- make it run)
-add(2,3);
-//greeter is the function identifier. name is the functions
-//argument (info it need todo its jobs)
-function add(num1,num2){
-    answer = num1 + num2;
-    console.log("the answer is:" + answer);
-    return answer;
+// // call function add (use it- make it run)
+// add(2,3);
+// //greeter is the function identifier. name is the functions
+// //argument (info it need todo its jobs)
+// function add(num1,num2){
+//     answer = num1 + num2;
+//     console.log("the answer is:" + answer);
+//     return answer;
+// }
+
+// function greeter(name){
+//     alert("Welcome to " + name + "data visualization")
+// }
+
+// greeter (" Rian Kilgore's")
+
+// var person = {name: "Rian", age: 32, car: {model: "Mazda", year: 2015}}
+// console.log ("My Name is " + person.name)
+// console.log ("My age is " + person.age)
+// console.log ("My car model is " + person.car.model)
+
+// if (person.age > 30) {
+//     alert ("you're older than 30")
+// }
+
+// //var year_list = []
+// var data = {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}]}
+// var year_list = data [2016]
+// console.log('year list is' + year_list) //will show as four objects.
+
+// console.log (year_list[0][9])
+// console.log (year_list[1][10])
+// console.log (year_list[2][11])
+// console.log (year_list[3][12])
+
+$(updateView)
+
+//array to hold data points pulled from jQuery
+let years= []
+let months2016 = []
+let months2017 = []
+let months2018 = []
+
+//define or implement a function
+function updateView(){
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear),
+  ).then(updateChart);
 }
 
-function greeter(name){
-    alert("Welcome to " + name + "data visualization")
-}
+function perYear(data) {
+    for (var index = 0, month=9; index <= 3, month<=12; ++month){
+      months2016.push(data[2016][index][month])
+    }
+    console.log("2016 data by months is easy" + months2016)
 
-greeter (" Rian Kilgore's")
+    for(var index = 0, month=1; index <=11, month <=12; ++index, ++month){
+    months2017.push(data[2017][index][month]);
+    }
+    console.log("2017 data by month is" + months2017)
 
-var person = {name: "Rian", age: 32, car: {model: "Mazda", year: 2015}}
-console.log ("My Name is " + person.name)
-console.log ("My age is " + person.age)
-console.log ("My car model is " + person.car.model)
-
-if (person.age > 30) {
-    alert ("you're older than 30")
-}
-
-//var year_list = []
-var data = {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}]}
-var year_list = data [2016]
-console.log('year list is' + year_list) //will show as four objects.
-
-console.log (year_list[0][9])
-console.log (year_list[1][10])
-console.log (year_list[2][11])
-console.log (year_list[3][12])
-
+    for(var index = 0, month=1; index <=data.length, month <= 10; ++index){
+    months2018.push(data[2018][index][month]);
+    }
+    console.log("2018 data by month is" + months2018)
+    
 // // function of update view 
 // function updateView() {
 //   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
