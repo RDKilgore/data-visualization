ff506cdc1c0823a1dd63c3d3e100ef0a5c5cab21
Rian <RDKilgore@gmail.com>
2019-12-02 15:17:50 -0800

    Starts to add new data

--- main_2.js
+++ main_2.js
@@ -14,31 +14,29 @@ var my_data = []
 //after the function perYear runs it will call the function updateChart()
 function updateView() {
   
-  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear), 
+  $.when ($.getJSON(BASE_URL + "/rides/count/per_hour", perHour), 
     ).then(updateChart);
 }
 
 
 // This is what your JSON data looks like
 //If you type https://zagster-service.herokuapp.com/rides/count/per_month in your web browser, you'll see this data
-// {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}],
-// "2017":[{"1":20},{"2":31},{"3":79},{"4":88},{"5":156},{"6":301},{"7":405},{"8":187},{"9":401},{"10":353},{"11":113},{"12":79}],
-// "2018":[{"1":94},{"2":83},{"3":154},{"4":1216},{"5":2400},{"6":1756},{"7":2748},{"8":1912},{"9":3648},{"10":2404}]}
+//{"0":429,"1":231,"2":130,"3":85,"4":42,"5":1,"7":1,"8":17,"9":16,"10":795,"11":425,"12":921,"13":846,"14":1795,"15":1789,"16":2119,"17":1630,"18":1942,"19":1637,"20":1636,"21":1054,"22":843,"23":710}
 
 // Funtion definition for perYear(data)
 // The data argument is your JSON data
 // This function extracts information from your JSON data and put it in an array called my_data
 // 
-function perYear(data) {
+function perHour(data) {
 
     // data2016 = [{"9":220},{"10":141},{"11":89},{"12":16}]
-    var data2016 = data[2016]  //extract the informatio associated with the key "2016" and put this info in a variable called data2016
+    var data = data[]  //extract the informatio associated with the key "2016" and put this info in a variable called data2016
 
     //extracting data for each month.We need to  specify which object and then specify the key before we can get the data for that key
     //So data2016[0][9] means look at the zero-ith array element {"9":220} and extract the data associated the key 9
     //result should be 220 
-    console.log(data2016[0][9]) //print daya to the console to make sure we did it right
-    my_data.push(data2016[0][9]) //add the data to the my_data array 
+    console.log(data[0]) //print daya to the console to make sure we did it right
+    my_data.push(data2016[0]) //add the data to the my_data array 
     console.log(data2016[1][10])  //looks at the one-ith array element {"10":141} extract data for key 10
     my_data.push(data2016[1][10])
     console.log(data2016[2][11])
