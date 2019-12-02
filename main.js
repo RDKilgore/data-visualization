a1ddadd08eb36086eeeefeb5d4644fd980f3228f
Rian <RDKilgore@gmail.com>
2019-12-02 14:47:36 -0800

    Update to add finalized JSON data by using instructor example.
    Exercise 16 complete

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 // //jQuery command. wait until web page loads call function 
 // //whos name is in parantheses
 // //function call means run the code
-// // $(updateGraph)
-// // $(updateView)
-// // $(greeter)
+// $(updateGraph)
+// $(updateView)
+// $(greeter)
 
 // // call function add (use it- make it run)
 // add(2,3);
@@ -16,9 +16,7 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 // function add(num1,num2){
 //     answer = num1 + num2;
 //     console.log("the answer is:" + answer);
-//     return answer;
-// }
-
+//     return answer;}
 // function greeter(name){
 //     alert("Welcome to " + name + "data visualization")
 // }
@@ -35,29 +33,29 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 // }
 
 // //var year_list = []
-// var data = {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}]}
-// var year_list = data [2016]
-// console.log('year list is' + year_list) //will show as four objects.
+var data = {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}]}
+var year_list = data [2016]
+console.log('year list is' + year_list) //will show as four objects.
 
-// console.log (year_list[0][9])
-// console.log (year_list[1][10])
-// console.log (year_list[2][11])
-// console.log (year_list[3][12])
+console.log (year_list[0][9])
+console.log (year_list[1][10])
+console.log (year_list[2][11])
+console.log (year_list[3][12])
 
 // $(updateView)
 $(updateGraph)
 //array to hold data points pulled from jQuery
 
-// let years= []
-// let months2016 = []
-// let months2017 = []
-// let months2018 = []
+let years= []
+let months2016 = []
+let months2017 = []
+let months2018 = []
 
 //define or implement a function
+$.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 function updateView(){
-  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-  
-  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear),).then(updateChart);
+    $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear),
+    ).then(updateChart);
 }
 
 function perYear(data) {
@@ -74,23 +72,23 @@ function perYear(data) {
     for(var index = 0, month=1; index <=data.length, month <= 10; ++index){
     months2018.push(data[2018][index][month]);
     }
-    console.log("2018 data by month is" + months2018)
-    
+    console.log("2018 data by month is" + months2018)}
 // // function of update view 
 // function updateView() {
 //   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 // }
 // //grabs the id ridecount  
-// function updateRideCount(data) {
-//   numberOfRides = data.count
-//   $("h3#rideCount").html(numberOfRides)
-// }
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h3#rideCount").html(numberOfRides)
+}
 
 // console.log(numberOfRides)
 
 //creates chart 
+
 function updateGraph() {
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('myChart').getContext('2d');}
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
@@ -105,7 +103,4 @@ var chart = new Chart(ctx, {
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
     },
-
-//     // Configuration options go here
-//     options: {}
-// });
+});    
