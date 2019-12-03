e41bd8354cc1dc2ab84aa5fd8329b320dc38ed31
Rian <RDKilgore@gmail.com>
2019-12-02 17:48:26 -0800

    Changes Graph to Bar graph

--- main_2.js
+++ main_2.js
@@ -20,7 +20,7 @@ function updateView() {
 
 
 // This is what your JSON data looks like
-//If you type https://zagster-service.herokuapp.com/rides/count/per_month in your web browser, you'll see this data
+//If you type https://zagster-service.herokuapp.com/rides/count/per_hour in your web browser, you'll see this data
 //{"0":429,"1":231,"2":130,"3":85,"4":42,"5":1,"7":1,"8":17,"9":16,"10":795,"11":425,"12":921,"13":846,"14":1795,"15":1789,"16":2119,"17":1630,"18":1942,"19":1637,"20":1636,"21":1054,"22":843,"23":710}
 
 // Funtion definition for perYear(data)
@@ -29,12 +29,6 @@ function updateView() {
 // 
 function perHour(data) {
 
-    // data2016 = [{"9":220},{"10":141},{"11":89},{"12":16}]
-    ///extract the informatio associated with the key "2016" and put this info in a variable called data2016
-
-    //extracting data for each month.We need to  specify which object and then specify the key before we can get the data for that key
-    //So data2016[0][9] means look at the zero-ith array element {"9":220} and extract the data associated the key 9
-    //result should be 220 
     hours = []
     for(i=0; i<24; i=i+1){
         console.log(data[i]) //print daya to the console to make sure we did it right
@@ -53,13 +47,13 @@ function updateChart(my_data, hours) {
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
-        type: 'line',
+        type: 'bar',
 
         // The data for our dataset
         data: {
             labels: hours,
             datasets: [{
-                label: 'Zagster Number of Rides Per Month in 2016',
+                label: 'Zagster Number of Rides Per hour',
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
                 //data: [0, 10, 5, 2, 20, 30, 45]
@@ -68,9 +62,12 @@ function updateChart(my_data, hours) {
         },
 
         // Configuration options go here
-        options: {}
+        options: {
+
+        }
     });
 }
+// Below is other form of for loop
 
 // console.log(data2017[0][1]) 
 // my_data.push(data2017[0][1]) 
